32b8b12d19c01d6edba8f24ba7b19d986e66dcca
MikeC <>
2019-10-26 13:58:59 -0600

    Adds Mike's 290 files to project; Updates .gitignore

---
+++ databaseInteraction.js
@@ -0,0 +1,33 @@
+/******************************************************************************************
+ * Author: Mike Cumberworth
+ * CS 290, Section 400 Web Development Summer 2019 
+ * 
+ * Assignment Database Interaction:
+ * https://dev.mysql.com/doc/refman/8.0/en/enum.html
+ * https://www.sitepoint.com/community/t/mysql-table-and-column-field-naming-conventions/1322
+ ******************************************************************************************/
+
+const bodyParser = require('body-parser');
+const db = require('mysql');
+const express = require('express');
+const hb = require('express-handlebars').create({defaultLayout : 'main'});
+const path = require('path');
+// let session = require('express-session');
+
+const app = express();
+const publicDirPath = path.join(__dirname, '/public');
+
+// app.use(session({secret:'MathIsFunTheySaid', path: "/form"}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(publicDirPath));
+app.use(require('./routes.js')); 
+
+app.engine('handlebars', hb.engine);
+
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.listen(app.get('port'), () => {
+  console.log('Express started on localhost:3000'); // http://flip3.engr.oregonstate.edu:60302/
+});
---
+++ public/js/table.js
@@ -0,0 +1,104 @@
+/******************************************************************
+Functions that build and/or manipulate the table  
+******************************************************************/
+
+function updateTable(){
+  let i = 0;
+  let len = this.entries.length;
+  for (i; i < len; i++){
+    let obj = this.entries[i];
+    this.addTableRow(obj);
+  }
+}
+
+function addTableRow(obj, index){
+  if (!index) { 
+    index = -1 
+  }
+  let tbody = document.getElementById('tbody');
+  let row = tbody.insertRow(index);
+  row.classList.add('table-entry');
+
+  let name = row.insertCell(0);
+  name.textContent = obj.exerciseName;
+
+  let numReps = row.insertCell(1);
+  numReps.textContent = obj.numReps;
+
+  let weight = row.insertCell(2);
+  weight.textContent = obj.weight;
+
+  let exerciseDate = row.insertCell(3);
+  exerciseDate.textContent = convertDateForDisplay(obj.exerciseDate);
+
+  let unit = row.insertCell(4);          
+  unit.textContent = obj.unit;
+
+  let edit = row.insertCell(5);
+  edit.textContent = "Edit";
+  edit.classList.add("edit-btn");
+  edit.addEventListener('click', this.editEntry.bind(this));
+
+  let del = row.insertCell(6);
+  del.textContent = "Delete";
+  del.classList.add("delete-btn");
+  del.addEventListener('click', this.deleteEntry.bind(this));
+  
+  scrollToRowAtIndex(index);
+  row.classList.add('row-highlight-animated');
+  setTimeout(() => row.classList.remove('row-highlight-animated'), 2000);
+}
+
+function editTableRowAtIndex(obj, index){
+  let tbody = document.getElementById('tbody');
+  
+  let row = tbody.rows[index]; 
+  row.classList.add('table-entry');
+
+  let name = row.cells[0];
+  name.textContent = obj.exerciseName;
+
+  let numReps = row.cells[1];
+  numReps.textContent = obj.numReps;
+
+  let weight = row.cells[2];
+  weight.textContent = obj.weight;
+
+  let exerciseDate = row.cells[3];
+  exerciseDate.textContent = convertDateForDisplay(obj.exerciseDate);
+
+  let unit = row.cells[4];          
+  unit.textContent = obj.unit;
+
+  scrollToRowAtIndex(index);
+  row.classList.add('row-highlight-animated');
+  setTimeout(() => row.classList.remove('row-highlight-animated'), 2000);
+
+}
+
+function removeTableRowAtIndex(index){
+  let tbody = document.getElementById('tbody');
+  let row = tbody.rows[index];
+  row.classList.add('row-delete-animated');
+  setTimeout(() => {
+    row.classList.remove('row-delete-animated')
+    tbody.deleteRow(index);
+  }, 300);
+}
+
+function scrollToRowAtIndex(index){
+  console.log('scrolling to row?', index); 
+  let tbody = document.querySelector('#tbody'); 
+  let rows = tbody.querySelectorAll('tr');
+  if (index == -1) {index = rows.length - 1}
+  
+  console.log(rows[index]);
+  rows[index].scrollIntoView({
+    behavior: 'smooth',
+    block: 'center'
+  });  
+  // let scrolledY = window.scrolledY;
+  // if (scrolledY) {
+  //   tbody.scroll(0, scrollY - theadHeight);
+  // }
+}
