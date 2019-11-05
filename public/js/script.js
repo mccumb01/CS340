e52e0c5ffb536a6571ba00af702bb25ef025d7e0
MikeC <>
2019-11-04 20:04:46 -0700

    Adds new genre input & btn to item form; Includes 'Add' buttons for all items in Search results; some CSS changes

--- public/js/script.js
+++ public/js/script.js
@@ -108,54 +108,74 @@ function getOtherDataFromItunes(){
         var header = document.createElement("thead");
         var tblBody = document.createElement("tbody");
         var titleRow = document.createElement("tr")
-        var titleCell1 = document.createElement("td")
+        var titleCell1 = document.createElement("th")
         var titleCellText1 = document.createTextNode("Poster")
-        var titleCell2 = document.createElement("td")
+        var titleCell2 = document.createElement("th")
         var titleCellText2 = document.createTextNode("Title")
-        var titleCell3 = document.createElement("td")
+        var titleCell3 = document.createElement("th")
         var titleCellText3 = document.createTextNode("Type")
-        var titleCell4 = document.createElement("td")
+        var titleCell4 = document.createElement("th")
         var titleCellText4 = document.createTextNode("Release Date")
+        var titleCell5 = document.createElement("th")
+        var titleCellText5 = document.createTextNode("Add to Queue")
         titleCell1.appendChild(titleCellText1);
         titleCell2.appendChild(titleCellText2);
         titleCell3.appendChild(titleCellText3);
         titleCell4.appendChild(titleCellText4);
+        titleCell5.appendChild(titleCellText5);
         titleRow.appendChild(titleCell1);
         titleRow.appendChild(titleCell2);
         titleRow.appendChild(titleCell3);
         titleRow.appendChild(titleCell4);
+        titleRow.appendChild(titleCell5);
         header.appendChild(titleRow);
         tbl.appendChild(header);
         output.appendChild(tbl);
-        result = json.results
-        result.forEach(function(item){
-        var row = document.createElement("tr")
-        var cell1 = document.createElement("td")
-        var cellText1 = document.createTextNode(item.trackCensoredName)
-        var cell2 = document.createElement("td")
-        var cellText2 = document.createTextNode(item.kind)
-        var cell3 = document.createElement("td")
-        var date = new Date(item.releaseDate)
-        var cellText3 = document.createTextNode(date.getFullYear())
-        var img = document.createElement('img')
-        var imgCell = document.createElement("td")
-        link_img=item.artworkUrl100
-        img.src = link_img
-        
-        cell1.appendChild(cellText1);
-        cell2.appendChild(cellText2);
-        cell3.appendChild(cellText3);
-        imgCell.appendChild(img);
-        row.appendChild(imgCell);
-        row.appendChild(cell1);
-        row.appendChild(cell2);
-        row.appendChild(cell3);
-        tblBody.appendChild(row);
-        tbl.appendChild(tblBody);
-        output.appendChild(tbl);
+        result = json.results;
 
-        // sets the border attribute of tbl to 2;
-        tbl.setAttribute("border", "2");
+        result.forEach(function(item, index){
+            var row = document.createElement("tr")
+            var cell1 = document.createElement("td")
+            var cellText1 = document.createTextNode(item.trackCensoredName)
+            var cell2 = document.createElement("td")
+            var cellText2 = document.createTextNode(item.kind)
+            var cell3 = document.createElement("td")
+            var date = new Date(item.releaseDate)
+            var cellText3 = document.createTextNode(date.getFullYear())
+            var img = document.createElement('img')
+            var imgCell = document.createElement("td")
+            link_img=item.artworkUrl100
+            img.src = link_img
+            
+            var addBtnCell = document.createElement('td');
+            var addBtn = document.createElement("button");
+            addBtn.textContent = "Add";
+            addBtn.setAttribute('align', 'center');
+            addBtn.setAttribute('type', 'button');
+            addBtn.setAttribute('id', 'result-' + (index + 1));
+            addBtn.setAttribute('class', 'add-btn');
+            addBtn.addEventListener('click', $event =>{
+                let id = $event.target.id.substring(7);
+                alert("Adding item " + id + " : '" + item.trackCensoredName + "' to the queue!");
+            });
+            addBtnCell.appendChild(addBtn);
+
+
+            cell1.appendChild(cellText1);
+            cell2.appendChild(cellText2);
+            cell3.appendChild(cellText3);
+            imgCell.appendChild(img);
+            row.appendChild(imgCell);
+            row.appendChild(cell1);
+            row.appendChild(cell2);
+            row.appendChild(cell3);
+            row.appendChild(addBtnCell);
+            tblBody.appendChild(row);
+            tbl.appendChild(tblBody);
+            output.appendChild(tbl);
+
+            // sets the border attribute of tbl to 2; // preferable to use a CSS class
+            tbl.setAttribute("border", "2");
         });
     }).catch(error => console.log("ERROR!"))
 
